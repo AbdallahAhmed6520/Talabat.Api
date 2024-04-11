14ae4f9f001d6f11ed2280f5925c332ccea90e6e
abdosedroho <abdosedroho@gmail.com>
2024-04-11 20:38:04 +0200

    Product Module - Entities

---
+++ Talabat.Core/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Talabat.Core.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PictureUrl { get; set; }
+        public decimal Price { get; set; }
+
+        //[ForeignKey(nameof(Product.Brand))]
+        public int ProductBrandId { get; set; }
+
+        public int ProductCategoryId { get; set; }
+        public ProductBrand Brand { get; set; }
+
+        public ProductCategory Category { get; set; }
+    }
+}
