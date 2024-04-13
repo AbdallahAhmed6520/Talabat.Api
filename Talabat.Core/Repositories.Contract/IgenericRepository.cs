620cd946966f8b7e02d4cd0a227fc2170369c394
abdosedroho <abdosedroho@gmail.com>
2024-04-13 07:42:02 +0200

    Generic Repository

---
+++ Talabat.Core/Repositories.Contract/IgenericRepository.cs
@@ -0,0 +1,11 @@
+﻿using Talabat.Core.Entities;
+
+namespace Talabat.Core.Repositories.Contract
+{
+    public interface IgenericRepository<T> where T : BaseEntity
+    {
+        Task<T?> GetAsync(int id);
+
+        Task<IEnumerable<T>> GetAllAsync();
+    }
+}
---
+++ Talabat.Repository/GenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories.Contract;
+using Talabat.Repository.Data;
+
+namespace Talabat.Repository
+{
+    public class GenericRepository<T> : IgenericRepository<T> where T : BaseEntity
+    {
+        private readonly StoreContext _dbContext;
+
+        public GenericRepository(StoreContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T?> GetAsync(int id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+    }
+}
